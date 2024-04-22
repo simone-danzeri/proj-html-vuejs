e51ebc27b2e43181e3042bca8ff85ca520ec0c3c
simone-danzeri <s.danzeri@icloud.com>
2024-04-22 18:01:16 +0200

    feat: AppDreamSection: sezione con cards e uomo che esulta ma senza macchia viola sotto

--- src/components/AppDreamSection.vue
+++ src/components/AppDreamSection.vue
@@ -65,9 +65,33 @@
             </div>
         </div>
         <!-- Sezione con ragazzo che esulta -->
-        <div class="row mt-5 pt-5">
-            <div class="col-6"></div>
-            <div class="col-6"></div>
+        <div class="row justify-content-between mt-5 pt-5">
+            <div class="col-5">
+                <div class="ms-img-container d-flex justify-content-end">
+                    <img class="ms-man" src="../assets/img/about-us-03-image-01.png" alt="">
+                </div>
+            </div>
+            <div class="col-5">
+                <p>DREAM WITH <span class="ms-gray">MAXCOACH</span></p>
+                <h1 class="mb-5">Construct A <span class="ms-green">Stunning</span> Career Perspective</h1>
+                <div class="ms-accordion-title-container ms-first d-flex justify-content-between py-3 px-4">
+                    <span>Multiple Platforms Supported for Teaching & Studying</span>
+                    <span><i class="fa-solid fa-circle-minus"></i></span>
+                </div>
+                <div class="ms-accordion-text">
+                    <p>
+                        MaxCoach supports the act of teaching and learning on multiple platforms like online or offline via material downloads. We know things aren't supposed to be devaured in a short time, you can always access our knowledge base from any device.
+                    </p>
+                </div>
+                <div class="ms-accordion-title-container-2 d-flex justify-content-between py-3 px-4">
+                    <span class="ms-accordion-title">Multiple Course Partecipation At The Same Time</span>
+                    <span class="ms-accordion-fa"><i class="fa-solid fa-circle-plus"></i></span>
+                </div>
+                <div class="ms-accordion-title-container-2 d-flex justify-content-between py-3 px-4 mt-4">
+                    <span class="ms-accordion-title">Multiple Course Partecipation At The Same Time</span>
+                    <span class="ms-accordion-fa"><i class="fa-solid fa-circle-plus"></i></span>
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -112,5 +136,41 @@
     p{
         color: $gray;
     }
+}
+
+.ms-img-container{
+    height: 550px;
+}
+
+.ms-gray{
+    color: $gray;
+    font-weight: 700;
+}
+
+.ms-green{
+    color: $green;
+    font-weight: 300;
+}
+
+.ms-accordion-title-container{
+    border-radius: 5px;
+}
+.ms-first{
+    background-color: $green;
+    color: white;
+}
+
+.ms-accordion-text{
+    padding: 20px 12px;
+    width: 80%;
+    background-color: white;
+}
+
+.ms-accordion-title-container-2{
+    border-radius: 5px;
+    color: $gray;
+    box-shadow: 0px 14px 35px 33px rgba(0,0,0,0.05);
+    -webkit-box-shadow: 0px 14px 35px 33px rgba(0,0,0,0.05);
+    -moz-box-shadow: 0px 14px 35px 33px rgba(0,0,0,0.05);
 }
 </style>
