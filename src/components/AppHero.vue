6d0b1641ab40b810da39758a6bfeef3bc8cd1f29
simone-danzeri <s.danzeri@icloud.com>
2024-04-22 14:04:49 +0200

    feat: aggiunte icone a destra mancanti

--- src/components/AppHero.vue
+++ src/components/AppHero.vue
@@ -7,16 +7,26 @@
 
 <template>
     <div class="ms-bg">
+        <div class="ms-side d-flex flex-column justify-content-center align-items-center gap-3 ">
+            <i class="fa-solid fa-ruler-combined"></i>
+            <i class="fa-solid fa-life-ring"></i>
+            <i class="fa-solid fa-book"></i>
+            <i class="fa-solid fa-cart-shopping"></i>
+        </div>
         <div class="container">
             <div class="row py-5">
                 <div class="col-6">
-                    <div class="ms-card">
+                    <div class="ms-card d-flex flex-column justify-content-center">
                         <h1>Complete Your Dreams In JavaS Programming</h1>
                         <p class="py-4">Learn any coding program in distance and for a reasonable fee. You don't have to struggle alone, you've got our assistance and help.</p>
                         <button> <i class="fa-solid fa-download pe-3"></i> Download free E-Book</button>
                     </div>
                 </div>
-                <div class="col-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa saepe doloribus ducimus dolore quidem vitae qui ab amet tempore numquam quae blanditiis pariatur neque expedita quod, perspiciatis officiis iste odio!</div>
+                <div class="col-6">
+                    <div class="ms-img-container">
+                        <img src="../assets/img/home-5-hero-image.png" alt="">
+                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -29,10 +39,23 @@
     background-image: url('../assets/img/jumbo-overlay.svg');
     background-repeat: no-repeat;
     background-size: cover;
+    background-position: center;
     width: 100%;
-    height: 400px;
+    position: relative;
+
+    .ms-side{
+        position: absolute;
+        top: 20%;
+        right: 0;
+        background-color: white;
+        padding: 9px 8px;
+        border-radius: 5px;
+
+    }
 
     .ms-card{
+        height: 100%;
+
         h1{
             width: 60%;
             color: $purple;
@@ -55,6 +78,14 @@
             &:hover{
                 background-color: $purple;
             }
+        }
+    }
+
+    .ms-img-container{
+        width: 80%;
+
+        img{
+            max-width: 100%;
         }
     }
 }
