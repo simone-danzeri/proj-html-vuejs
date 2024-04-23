82dfca12510d7a3abb9fdc8048e0b477621ddf1d
simone-danzeri <s.danzeri@icloud.com>
2024-04-23 12:05:19 +0200

    feat: AppContacts: creata struttura dati in PageMain

---
+++ src/components/AppContacts.vue
@@ -0,0 +1,36 @@
+<script>
+    export default {
+        name: 'AppContacts'
+    }
+</script>
+
+
+<template>
+    <!-- Sezione con eventi -->
+    <section>
+        <div class="container">
+            <div class="ms-title-wrapper text-center py-5">
+                <small class="ms-thin ms-small">GET IN CONTACT NOW</small>
+                <h2 class="ms-purple py-4">Upcoming <span class="ms-green">Events</span></h2>
+            </div>
+        </div>
+    </section>
+</template>
+
+
+<style scoped lang="scss">
+@use '../style/partials/variables' as *;
+/* STILI */
+.ms-green{
+    color: $green;
+}
+.ms-purple{
+    color: $purple;
+}
+.ms-thin{
+    font-weight: 300;
+}
+.ms-small{
+    font-size: 12px;
+}
+</style>
--- src/components/PageMain.vue
+++ src/components/PageMain.vue
@@ -1,11 +1,55 @@
 <script>
     import AppDreamSection from './AppDreamSection.vue';
     import AppCourses from './AppCourses.vue';
+    import AppContacts from './AppContacts.vue'
     export default {
         name: 'PageMain',
         components: {
             AppDreamSection,
-            AppCourses
+            AppCourses,
+            AppContacts
+        },
+        data() {
+            return {
+                events: [
+                    {
+                        where: 'Texas, US',
+                        title: 'Storytelling Workshop',
+                        day: 22,
+                        month: 'NOV'
+                    },
+                    {
+                        where: 'New York, US',
+                        title: 'Painting Art Contest 2024',
+                        day: 10,
+                        month: 'OCT'
+                    },
+                    {
+                        where: 'Hamburg, Germany',
+                        title: 'International Art Fair 2024',
+                        day: 23,
+                        month: 'NOV'
+                    },
+                    {
+                        where: 'Illinois, US',
+                        title: 'Street Performance: Call for Artist',
+                        day: 15,
+                        month: 'DEC'
+                    },
+                    {
+                        where: 'Illinois, US',
+                        title: 'Consumer Food Safety Education Conference',
+                        day: 22,
+                        month: 'JUL'
+                    },
+                    {
+                        where: 'Dubai',
+                        title: 'How meditation improve your mental health?',
+                        day: 12,
+                        month: 'AUG'
+                    },
+                ],
+            };
         }
     }
 </script>
@@ -14,6 +58,7 @@
 <template>
     <AppDreamSection></AppDreamSection>
     <AppCourses></AppCourses>
+    <AppContacts></AppContacts>
 </template>
 
 
